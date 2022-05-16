5af9e91b73bb30f3d65dad898e9152a0a51fa8b0
Gennady Badinov <gennady.badinov@gmail.com>
2022-05-16 14:21:54 +0300

    Display multiple stars on screen

--- main.py
+++ main.py
@@ -1,16 +1,46 @@
 import asyncio
-import time
 import curses
+import random
+import time
+from typing import Coroutine
 
 TIC_TIMEOUT = 0.1
 
 
+def get_stars(
+    n: int, symbols: str, max_height: int, max_width: int, offset: int
+) -> list[tuple]:
+    return [
+        (
+            random.choice(list(symbols)),
+            random.randint(offset, max_height - offset),
+            random.randint(offset, max_width - offset),
+        )
+        for _ in range(n)
+    ]
+
+
+def get_courutines(canvas, stars: list[tuple]) -> list[Coroutine]:
+    coroutines = []
+    for star in stars:
+        symbol, row, column = star
+        coroutines.append(blink(canvas, row, column, symbol))
+    return coroutines
+
+
 def draw(canvas):
-    row, column = (5, 20)
     curses.curs_set(False)
     canvas.border()
 
-    coroutines = [blink(canvas, row, column + i) for i in range(0, 25, 5)]
+    canvas_height, canvas_width = curses.window.getmaxyx(canvas)
+    stars = get_stars(
+        n=150,
+        symbols="+*.:",
+        max_height=canvas_height,
+        max_width=canvas_width,
+        offset=3,  # offset from canvas borders
+    )
+    coroutines = get_courutines(canvas, stars)
 
     while True:
         for coroutine in coroutines:
@@ -19,7 +49,7 @@ def draw(canvas):
         time.sleep(TIC_TIMEOUT)
 
 
-async def blink(canvas, row, column, symbol="*"):
+async def blink(canvas, row, column, symbol="*") -> Coroutine:
     while True:
         canvas.addstr(row, column, symbol, curses.A_DIM)
         for _ in range(20):
