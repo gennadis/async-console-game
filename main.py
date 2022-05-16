49741e8e3630fe9ac0952d38207649dc29e00e27
Gennady Badinov <gennady.badinov@gmail.com>
2022-05-16 13:16:05 +0300

    Blink star with async finction

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import asyncio
 import time
 import curses
 
@@ -6,27 +7,43 @@ def draw(canvas):
     row, column = (5, 20)
     curses.curs_set(False)
     canvas.border()
-    
+
+    coroutine = blink(canvas, row, column)
+
+    coroutine.send(None)
+    canvas.refresh()
+    time.sleep(2)
+
+    coroutine.send(None)
+    canvas.refresh()
+    time.sleep(0.3)
+
+    coroutine.send(None)
+    canvas.refresh()
+    time.sleep(0.5)
+
+    coroutine.send(None)
+    canvas.refresh()
+    time.sleep(0.3)
+
+    time.sleep(3)
+
+
+async def blink(canvas, row, column, symbol="*"):
     while True:
-        canvas.addstr(row, column, '*', curses.A_DIM)
-        canvas.refresh()
-        time.sleep(2)
+        canvas.addstr(row, column, symbol, curses.A_DIM)
+        await asyncio.sleep(0)
 
-        canvas.addstr(row, column, '*')
-        canvas.refresh()
-        time.sleep(0.3)
+        canvas.addstr(row, column, symbol)
+        await asyncio.sleep(0)
 
-        canvas.addstr(row, column, '*', curses.A_BOLD)
-        canvas.refresh()
-        time.sleep(0.5)
+        canvas.addstr(row, column, symbol, curses.A_BOLD)
+        await asyncio.sleep(0)
 
-        canvas.addstr(row, column, '*')
-        canvas.refresh()
-        time.sleep(0.3)
+        canvas.addstr(row, column, symbol)
+        await asyncio.sleep(0)
 
 
-  
-if __name__ == '__main__':
+if __name__ == "__main__":
     curses.update_lines_cols()
     curses.wrapper(draw)
-    
