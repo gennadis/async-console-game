b56b832be53aebe352528f537bf6212c82459854
Gennady Badinov <gennady.badinov@gmail.com>
2022-05-16 16:10:41 +0300

    Draw rocket animation

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import asyncio
 import curses
 import random
 import time
+from types import coroutine
 from typing import Coroutine
 
 TIC_TIMEOUT = 0.1
@@ -20,7 +21,7 @@ def get_stars(
     ]
 
 
-def get_courutines(canvas, stars: list[tuple]) -> list[Coroutine]:
+def get_stars_courutines(canvas, stars: list[tuple]) -> list[Coroutine]:
     coroutines = []
     for star in stars:
         symbol, row, column = star
@@ -40,12 +41,18 @@ def draw(canvas):
         max_width=canvas_width,
         offset=3,  # offset from canvas borders
     )
-    coroutines = get_courutines(canvas, stars)
+    stars_coroutines = get_stars_courutines(canvas, stars)
+    rocket = fire(canvas, int(canvas_height / 2), int(canvas_width / 2))
+    coroutines = [rocket, *stars_coroutines]
 
     while True:
-        for coroutine in coroutines:
-            coroutine.send(None)
-            canvas.refresh()
+        for coroutine in coroutines.copy():
+            try:
+                coroutine.send(None)
+            except StopIteration:
+                coroutines.remove(coroutine)
+
+        canvas.refresh()
         time.sleep(TIC_TIMEOUT)
 
 
@@ -67,6 +74,42 @@ async def blink(canvas, row, column, symbol="*") -> Coroutine:
         canvas.addstr(row, column, symbol)
         for _ in range(3):
             await asyncio.sleep(0)
+
+
+async def fire(
+    canvas,
+    start_row: int,
+    start_column: int,
+    rows_speed: float = -0.3,
+    columns_speed: int = 0,
+):
+    """Display animation of gun shot, direction and speed can be specified."""
+
+    row, column = start_row, start_column
+
+    canvas.addstr(round(row), round(column), "*")
+    await asyncio.sleep(0)
+
+    canvas.addstr(round(row), round(column), "O")
+    await asyncio.sleep(0)
+    canvas.addstr(round(row), round(column), " ")
+
+    row += rows_speed
+    column += columns_speed
+
+    symbol = "-" if columns_speed else "|"
+
+    rows, columns = canvas.getmaxyx()
+    max_row, max_column = rows - 1, columns - 1
+
+    curses.beep()
+
+    while 0 < row < max_row and 0 < column < max_column:
+        canvas.addstr(round(row), round(column), symbol)
+        await asyncio.sleep(0)
+        canvas.addstr(round(row), round(column), " ")
+        row += rows_speed
+        column += columns_speed
 
 
 if __name__ == "__main__":
